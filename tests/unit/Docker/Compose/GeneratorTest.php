dde07e7cc9320e1996a19043f21089147f5ed0ba
Torsten Dittmann <torsten.dittmann@proton.me>
2026-09-06 13:38:02 +0000

    Add Autogravity health check

--- tests/unit/Docker/Compose/GeneratorTest.php
+++ tests/unit/Docker/Compose/GeneratorTest.php
@@ -69,7 +69,8 @@ final class GeneratorTest extends TestCase
         $this->assertArrayHasKey('appwrite-task-interval', $compose['services']);
         $this->assertArrayHasKey('appwrite-embedding', $compose['services']);
         $this->assertArrayHasKey('appwrite-autogravity', $compose['services']);
-        $this->assertSame('ghcr.io/appwrite/autogravity:0.0.2', $compose['services']['appwrite-autogravity']['image']);
+        $this->assertSame('ghcr.io/appwrite/autogravity:0.0.3', $compose['services']['appwrite-autogravity']['image']);
+        $this->assertStringContainsString('/healthz', $compose['services']['appwrite-autogravity']['healthcheck']['test'][3]);
         $this->assertArrayNotHasKey('profiles', $compose['services']['appwrite-worker']);
         $this->assertArrayNotHasKey('profiles', $compose['services']['appwrite-task-scheduler']);
     }
